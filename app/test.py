3734936653226bc7357f9bd0626d3c5b45a130c2
xoos <xoos0420@gmail.com>
2024-04-18 20:13:33 +0900

    Add something

--- app/test.py
+++ app/test.py
@@ -1,15 +1,3 @@
-import pandas as pd
-
-# 데이터셋을 불러옵니다.
-data = {
-    'userId': [1, 1, 2, 2],
-    'movieId': [1, 2, 1, 2],
-    'rating': [4, 3, 5, 4]
-}
-df = pd.DataFrame(data)
-
-# userId와 movieId를 범주형으로 변환합니다.
-df['userId'] = df['userId'].astype('category')
-df['movieId'] = df['movieId'].astype('category')
-
-print(df)
+import os
+api_key = os.environ.get('TMDB_API_KEY')
+print(api_key)
